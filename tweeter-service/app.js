3b368b57f0b812fc446b2782febbbb251f3dee8a
Adina Shanholtz <adinas@microsoft.com>
2017-04-25 18:40:50 -0400

    wrote documentation

--- tweeter-service/app.js
+++ tweeter-service/app.js
@@ -19,17 +19,28 @@ var options = {
 //Twitter
 var T = new Twit(options);
 
+/**
+ * @api {get} / Root get method but only returns hello world.
+ */
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
- 
+ /**
+  * @api {post} /api/tweet Tweets a video file 
+  * @apiParam {Object} "filepath": "[PATH_TO_FILE]" Expects a JSON object that'll designate the file path of the video to be tweeted. 
+  * @apiError Any errors here will return 500 caused by incrorrect JSON object filepath, or errors trying to post to twitter.
+  */
 app.post('/api/tweet', function (req, res) {
     console.log(req.body);
   
     //Posts media to twitter in chunks
     T.postMediaChunked({ file_path: req.body.filepath }, function (err, data, response) {
          
+         if(err){
+             res.status(500).send({ error: 'Something failed with chunking the media. You probably messed up the filepath object.' });
+             console.log(err);
+         }
           var mediaIdStr = data.media_id_string;
           var altText = "Small flowers in a planter on a sunny balcony, blossoming.";
           var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
@@ -41,8 +52,15 @@ app.post('/api/tweet', function (req, res) {
                 var params = { status: 'Hop in bith, we\'re going to the solar system', media_ids: [mediaIdStr] };
 
                  T.post('statuses/update', params, function (err, data, response) {
+                    if(err){
+                        res.sendStatus(500, err);
+                        return;
+                    }
                     res.send("cool");
                })
+            }
+            else{
+                res.sendStatus(500, err);
             }
         })
     })
