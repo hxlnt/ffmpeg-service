7b41067de2d338058c0fecc4444b3f7c45876e5f
Suz Hinton <suz.hinton@gmail.com>
2017-04-26 10:52:44 -0500

    break out tweet functionality

--- tweeter-service/app.js
+++ tweeter-service/app.js
@@ -1,23 +1,12 @@
-
-var Twit = require('twit');
+var tweeter = require('./lib/tweet');
 var express = require('express');
 var bodyParser = require('body-parser'); 
 
+tweeter.init();
 
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json 
 
-//ENV variables
-var options = {
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret,
-    timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
-}
-
-//Twitter
-var T = new Twit(options);
 
 /**
  * @api {get} / Root get method but only returns hello world.
@@ -33,41 +22,17 @@ app.get('/', function (req, res) {
   */
 app.post('/api/tweet', function (req, res) {
     console.log(req.body);
-  
-    //Posts media to twitter in chunks
-    T.postMediaChunked({ file_path: req.body.filepath }, function (err, data, response) {
-         
-         if(err){
-             res.status(500).send({ error: 'Something failed with chunking the media. You probably messed up the filepath object.' });
-             console.log(err);
-         }
-          var mediaIdStr = data.media_id_string;
-          var altText = "Small flowers in a planter on a sunny balcony, blossoming.";
-          var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
-
-          T.post('media/metadata/create', meta_params, function (err, data, response) {
-            if (!err) {
-
-                // now we can reference the media and post a tweet (media will attach to the tweet)
-                var params = { status: 'Hop in bith, we\'re going to the solar system', media_ids: [mediaIdStr] };
-
-                 T.post('statuses/update', params, function (err, data, response) {
-                    if(err){
-                        res.sendStatus(500, err);
-                        return;
-                    }
-                    res.send("cool");
-               })
-            }
-            else{
-                res.sendStatus(500, err);
-            }
-        })
-    })
-
+    var filepath = req.body.filepath;
+    tweeter.tweet(filepath, function(error) {
+        if (error) {
+          res.status(500).send({error});
+        } else {
+          res.send('cool');
+        }
+    });
 });
 
 var port = process.env.TWEETER_PORT || 3002;
 app.listen(port);
-
+console.log('listening on: ' + port);
 
