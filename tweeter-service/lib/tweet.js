5a51c7dc7deb20390483e396516e6d0bf8cb71b2
Suz Hinton <suz.hinton@gmail.com>
2017-04-26 11:15:27 -0500

    add hashtag to tweet status

--- tweeter-service/lib/tweet.js
+++ tweeter-service/lib/tweet.js
@@ -30,7 +30,7 @@ module.exports.tweet = (filepath, callback) => {
         T.post('media/metadata/create', meta_params, (err) => {
           if (!err) {
               // now we can reference the media and post a tweet (media will attach to the tweet)
-              const params = { status: 'Here\'s a nice video for you', media_ids: [mediaIdStr] };
+              const params = { status: 'escape your world with Sun Ra #hackchi', media_ids: [mediaIdStr] };
 
               T.post('statuses/update', params, (err) => {
                 return callback(err);
