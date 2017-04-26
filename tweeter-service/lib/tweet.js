c75b0e3dcce75274eb1330ed5df302230f9d1db6
Suz Hinton <suz.hinton@gmail.com>
2017-04-26 11:17:16 -0500

    fix indentation

--- tweeter-service/lib/tweet.js
+++ tweeter-service/lib/tweet.js
@@ -2,13 +2,13 @@ const Twit = require('twit');
 let T;
 
 module.exports.init = () => {
-  //ENV variables
+  // ENV variables
   const options = {
-      consumer_key: process.env.consumer_key,
-      consumer_secret: process.env.consumer_secret,
-      access_token: process.env.access_token,
-      access_token_secret: process.env.access_token_secret,
-      timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret,
+    timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
   }
   // new Twitter connection
   T = new Twit(options);
@@ -16,28 +16,27 @@ module.exports.init = () => {
 
 module.exports.tweet = (filepath, callback) => {
   //Posts media to twitter in chunks
-  T.postMediaChunked({ file_path: filepath }, (err, data) => {
-        
-        if (err) {
-          console.log(err);
-          return callback(new Error('Something failed with chunking the media. You probably messed up the filepath object.'))  
-        }
+  T.postMediaChunked({ file_path: filepath }, (err, data) => {  
+    if (err) {
+      console.log(err);
+      return callback(new Error('Something failed with chunking the media. You probably messed up the filepath object.'))  
+    }
 
-        const mediaIdStr = data.media_id_string;
-        const altText = 'cool generative video with random text and imagery';
-        const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+    const mediaIdStr = data.media_id_string;
+    const altText = 'cool generative video with random text and imagery';
+    const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
 
-        T.post('media/metadata/create', meta_params, (err) => {
-          if (!err) {
-              // now we can reference the media and post a tweet (media will attach to the tweet)
-              const params = { status: 'escape your world with Sun Ra #hackchi', media_ids: [mediaIdStr] };
+    T.post('media/metadata/create', meta_params, (err) => {
+      if (!err) {
+          // now we can reference the media and post a tweet (media will attach to the tweet)
+          const params = { status: 'escape your world with Sun Ra #hackchi', media_ids: [mediaIdStr] };
 
-              T.post('statuses/update', params, (err) => {
-                return callback(err);
-              });
-          } else {
+          T.post('statuses/update', params, (err) => {
             return callback(err);
-          }
-      })
+          });
+      } else {
+        return callback(err);
+      }
+    });
   })
 };
