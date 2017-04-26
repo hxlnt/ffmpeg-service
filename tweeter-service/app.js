0bdad7c82c32d7f16a9ea172af38bf6aca4f0d06
Bryan Hughes <bryan@theoreticalideations.com>
2017-04-26 14:44:02 -0500

    Added twitter username

--- tweeter-service/app.js
+++ tweeter-service/app.js
@@ -1,11 +1,11 @@
 const tweeter = require('./lib/tweet');
 const express = require('express');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
 tweeter.init();
 
 const app = express();
-app.use(bodyParser.json()); // for parsing application/json 
+app.use(bodyParser.json()); // for parsing application/json
 
 
 /**
@@ -16,13 +16,14 @@ app.get('/', ([...res]) => {
 });
 
  /**
-  * @api {post} /api/tweet Tweets a video file 
-  * @apiParam {Object} "filepath": "[PATH_TO_FILE]" Expects a JSON object that'll designate the file path of the video to be tweeted. 
+  * @api {post} /api/tweet Tweets a video file
+  * @apiParam {Object} "filepath": "[PATH_TO_FILE]" Expects a JSON object that'll designate the file path of the video to be tweeted.
   * @apiError Any errors here will return 500 caused by incrorrect JSON object filepath, or errors trying to post to twitter.
   */
 app.post('/api/tweet', (req, res) => {
+    const username = req.body.username;
     const filepath = req.body.filepath;
-    tweeter.tweet(filepath, (error) => {
+    tweeter.tweet(username, filepath, (error) => {
         if (error) {
           res.status(500).send({error});
         } else {
--- tweeter-service/lib/tweet.js
+++ tweeter-service/lib/tweet.js
@@ -14,7 +14,7 @@ module.exports.init = () => {
   T = new Twit(options);
 };
 
-module.exports.tweet = (filepath, callback) => {
+module.exports.tweet = (username, filepath, callback) => {
   //Posts media to twitter in chunks
   T.postMediaChunked({ file_path: filepath }, (err, data) => {
     if (err) {
@@ -29,7 +29,7 @@ module.exports.tweet = (filepath, callback) => {
     T.post('media/metadata/create', meta_params, (err) => {
       if (!err) {
           // now we can reference the media and post a tweet (media will attach to the tweet)
-          const params = { status: 'escape your world with Sun Ra #hackchi', media_ids: [mediaIdStr] };
+          const params = { status: `@${username} escape your world with Sun Ra #hackchi`, media_ids: [mediaIdStr] };
 
           T.post('statuses/update', params, (err) => {
             return callback(err);
